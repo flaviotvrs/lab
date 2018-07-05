6257ef4301d4057355773b9211580e7a7af42081
Rangel Soares de Souza <rangel@cit.local>
2018-07-05 14:04:36 -0300

    testes

--- src/test/java/com/ciandt/lab/LabApplicationTests.java
+++ src/test/java/com/ciandt/lab/LabApplicationTests.java
@@ -1,5 +1,6 @@
 package com.ciandt.lab;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,8 @@ public class LabApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		int soma = 2+2;
+		Assert.assertTrue(5 > soma);
 	}
 
 }
